778ec1ecca75b6fc664e40bcec82f4f46717a336
tymondragon <tymondragon@gmail.com>
2021-04-23 12:51:13 -0600

    update list to actual directories

--- lib/directory_analyzer_web/views/directory_view.ex
+++ lib/directory_analyzer_web/views/directory_view.ex
@@ -1,11 +1,14 @@
 defmodule DirectoryAnalyzerWeb.DirectoryView do
   use DirectoryAnalyzerWeb, :view
 
-  @list ["lorem", "ipsum", "zombies"]
+  @list ["lorem", "recipes", "the_living_mummy"]
   def directoryist(directories) when length(directories) === 0, do: @list
 
   def directory_list(directories) do
-    directories = Enum.map(directories, &Map.get(&1, :name)) |> Enum.map(&String.downcase(&1))
+    directories =
+      Enum.map(directories, &Map.get(&1, :name))
+      |> Enum.map(&String.downcase(&1))
+      |> Enum.map(&String.replace(&1, "_", " "))
 
     Enum.filter(@list, fn folder ->
       !Enum.member?(directories, folder)
