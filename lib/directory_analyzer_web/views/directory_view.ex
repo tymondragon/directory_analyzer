808ae8cb2382a8d695ca0a7e971c589ef35d63ed
tymondragon <tymondragon@gmail.com>
2021-04-23 07:42:24 -0600

    add link logic for each "directory"

--- lib/directory_analyzer_web/views/directory_view.ex
+++ lib/directory_analyzer_web/views/directory_view.ex
@@ -2,12 +2,12 @@ defmodule DirectoryAnalyzerWeb.DirectoryView do
   use DirectoryAnalyzerWeb, :view
 
   @list ["lorem", "ipsum", "zombies"]
-  def directoryList(directories) when length(directories) === 0, do: @list
+  def directoryist(directories) when length(directories) === 0, do: @list
 
-  def directoryList(directories) do
+  def directory_list(directories) do
     directories = Enum.map(directories, &Map.get(&1, :name)) |> Enum.map(&String.downcase(&1))
     Enum.filter(@list, fn folder ->
-      !Enum.member?(directories, String.downcase(folder))
+      !Enum.member?(directories, folder)
     end)
   end
 end
